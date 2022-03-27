ef9c8f5374288e083bc602b7b80b369dd68033d9
PRABHU SUNDARRAJ <43057160+prabhus489@users.noreply.github.com>
2022-03-27 12:03:16 +0530

    initial tf learning changes

---
+++ tflabs-modules/modules/registry-module/registry-module.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIA5RSSUULDTSMJCJEN"
+  secret_key = "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"
+}
+
+module "ec2_cluster" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 2.0"
+
+  name           = "my-cluster"
+  instance_count = 1
+
+  ami           = "ami-0c2d06d50ce30b442"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-c968db83"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+#https://registry.terraform.io/modules/terraform-aws-modules/ec2-instance/aws/2.21.0
---
+++ tflabs-modules/projects/A/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIA5RSSUULDTSMJCJEN"
+  secret_key = "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"
+}
